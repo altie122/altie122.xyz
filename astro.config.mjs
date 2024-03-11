4395a81b2d610356e16663683832b027d2115118
αltie <77561831+altie122@users.noreply.github.com>
2024-03-11 19:56:33 +0000

    webmanifest fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import webmanifest from "astro-webmanifest";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://altie122.dovahkiin.xyz',
-  integrations: [tailwind(), sitemap(), webmanifest({
-    name: 'Altie122',
-    icon: '/icon.png',
-    short_name: 'Altie122',
-    description: 'Altie122\'s website',
-    start_url: '/',
-    theme_color: '#450a0a',
-    background_color: '#0f172a',
-    display: 'standalone',
-  }),
-  ]
+  integrations: [tailwind(), sitemap(),]
 });
