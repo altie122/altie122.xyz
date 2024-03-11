db312ea6e4a8515850881b577c80dab1679391b2
αltie <77561831+altie122@users.noreply.github.com>
2024-03-11 19:34:39 +0000

    add webmanifest generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,15 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://altie122.dovahkiin.xyz',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), webmanifest({
+    name: 'Altie122',
+    icon: '/icon.png',
+    short_name: 'Altie122',
+    description: 'Altie122\'s website',
+    start_url: '/',
+    theme_color: '#450a0a',
+    background_color: '#0f172a',
+    display: 'standalone',
+  }),
+  ]
 });
