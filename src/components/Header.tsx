a7ff0290ed2c2012feb46af8a0e230ae2f40c6fd
altie122 <77561831+altie122@users.noreply.github.com>
2024-06-02 18:36:36 -0600

    add mode toggle and center nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "./ModeToggle";
 import { NavDrawer } from "./NavDrawer";
 import { Button } from "./ui/button";
 
@@ -6,9 +7,10 @@ export default function Header() {
     window.location.href = url;
   };
   return (
-    <header className="bg-secondary text-secondary-foreground h-16 flex items-center p-5">
-      <Button variant="outline" onClick={() => handleClick("/")} size='lg'>Altie122</Button>
+    <header className="bg-secondary text-secondary-foreground h-16 flex items-center p-5 justify-center">
       <NavDrawer />
+      <Button variant="outline" onClick={() => handleClick("/")} size='lg'>Altie122</Button>
+      <ModeToggle />
     </header>
   );
 }
