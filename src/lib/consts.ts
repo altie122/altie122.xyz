8e9d4283482978dd73ee8e4a02e33ecb5fed1c4e
altie122 <77561831+altie122@users.noreply.github.com>
2024-06-03 12:05:05 -0600

    add schedule to nav drawer

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -14,4 +14,9 @@ export const pages = [ // All STATIC pages, dynamic pages like blog posts are no
     href: 'https://yt-to-ytnocookie.dovahkiin.xyz/',
     isExternal: true,
   },
+  {
+    name: 'Schedule',
+    href: 'https://altie122.notion.site/459e425befd54b3894c9d282b2aa182f?v=daa59f8502d24644ba93b9aec3c09324&pvs=74',
+    isExternal: true,
+  },
 ]
