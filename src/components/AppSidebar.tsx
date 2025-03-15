547c23d2afb5ce0f7876895f48e3e99531d9c9ca
altie122 <altielynn@gmail.com>
2025-03-15 17:46:48 -0600

    add missing files

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,115 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarSeparator,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import {
+  ChevronDown,
+  PanelLeft,
+  Newspaper,
+  BookCheck,
+  NotebookPen,
+  LibraryBig,
+  Code,
+  Package,
+  Swords,
+  Server,
+  Image,
+  ScrollText,
+  BookUser,
+  BookOpen,
+  Eye,
+} from "lucide-react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+import React from "react";
+import { pages } from "~/lib/consts";
+
+export function AppSidebar() {
+  const { toggleSidebar } = useSidebar();
+  const [sidebar_collapsible_1_open, sidebar_collapsible_1_setOpen] =
+    useSidebarState(1);
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+      <SidebarHeader />
+      <SidebarContent>
+        <Collapsible
+          className="group/collapsible"
+          open={sidebar_collapsible_1_open}
+          onOpenChange={sidebar_collapsible_1_setOpen}
+        >
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                Pages
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {pages.map((item) => (
+                    <SidebarMenuItem key={item.name}>
+                      <SidebarMenuButton asChild>
+                        <a href={item.href}>
+                          <item.icon />
+                          <span>{item.name}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+      </SidebarContent>
+      <SidebarSeparator />
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton onClick={toggleSidebar}>
+              <PanelLeft />
+              <span className="ml-auto">Toggle Sidebar</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
+
+// track sidebar state
+function useSidebarState(collapsibleNumber: number) {
+  const [isOpen, setIsOpen] = React.useState(() => {
+    const savedOpenState = sessionStorage.getItem(
+      `main-sidebar-collapsible-${collapsibleNumber}Open`,
+    );
+    return savedOpenState ? JSON.parse(savedOpenState) : true;
+  });
+
+  React.useEffect(() => {
+    sessionStorage.setItem(
+      `main-sidebar-collapsible-${collapsibleNumber}Open`,
+      JSON.stringify(isOpen),
+    );
+  }, [isOpen, collapsibleNumber]);
+
+  return [isOpen, setIsOpen] as const; // Explicitly return a tuple
+}
---
+++ src/layouts/Root.tsx
@@ -0,0 +1,30 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "~/components/AppSidebar";
+import React from "react";
+import Navbar from "@/components/NavBar";
+// import Progress from "./progressbar";
+
+const Progress = () => {
+  // This is a placeholder for the progress bar, may or may not be implemented later
+  return null;
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [open, setOpen] = React.useState(() => {
+    const savedOpenState = sessionStorage.getItem("main-sidebarOpen");
+    return savedOpenState ? JSON.parse(savedOpenState) : true;
+  });
+  React.useEffect(() => {
+    sessionStorage.setItem("main-sidebarOpen", JSON.stringify(open));
+  }, [open]);
+  return (
+    <SidebarProvider open={open} onOpenChange={setOpen}>
+      <Progress />
+      <AppSidebar />
+      <main className="flex flex-col w-full h-full">
+        <Navbar />
+        <div className="h-[calc(100vh-3rem)]">{children}</div>
+      </main>
+    </SidebarProvider>
+  );
+}
