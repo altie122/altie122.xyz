5ec409a8ae83e9003c2df563bbd2a2625ec82538
altie122 <git@altie122.xyz>
2025-07-31 22:41:51 -0600

    try to fix build

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next"],
+    ignores: [".next", "old_files"],
   },
   ...compat.extends("next/core-web-vitals"),
   {
